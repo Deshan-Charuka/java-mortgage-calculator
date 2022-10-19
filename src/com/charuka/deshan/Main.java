8b117c355053c11c4ef7d93f6f05a16ad572ae3b
Deshan Charuka <d.c.0729439631@gmail.com>
2022-10-20 00:38:54 +0530

    Add Error handling

--- src/com/charuka/deshan/Main.java
+++ src/com/charuka/deshan/Main.java
@@ -12,21 +12,43 @@ public class Main {
     public static void main(String[] args) {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
-
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = (annualInterest/PERCENT)/MONTHS_IN_YEAR;
-        System.out.print("Period (Years): ");
-        byte periodMonths = (byte) (scanner.nextByte() * MONTHS_IN_YEAR);
+        int principal;
+        float annualInterest,monthlyInterest;
+        short periodMonths;
+
+
+        while(true){
+            System.out.print("Principal($1K - $1M): ");
+            principal = scanner.nextInt();
+            if(principal >= 1000 && principal <= 1_000_000)
+                break;
+            else
+                System.out.println("Please enter a value between 1,000 and 1,000,000");
+        }
+
+        while(true){
+            System.out.print("Annual Interest Rate: ");
+            annualInterest = scanner.nextFloat();
+            if(annualInterest > 0 && annualInterest <= 30){
+                monthlyInterest = (annualInterest/PERCENT)/MONTHS_IN_YEAR;
+                break;
+            }else
+                System.out.println("Please enter a value greater than 0 and less than or equal to 30");
+        }
+
+        while(true){
+            System.out.print("Period (Years): ");
+            periodMonths = (short) (scanner.nextByte() * MONTHS_IN_YEAR);
+            if(periodMonths >= 12 && periodMonths <= 360)
+                break;
+            else
+                System.out.println("Enter a value between 1 and 30");
+        }
 
         //Calculation
         double mortgage = principal * (monthlyInterest* Math.pow((1+monthlyInterest),periodMonths)/(Math.pow((1+monthlyInterest),periodMonths)-1));
-
         String formattedMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
-
         System.out.println("Mortgage: "+formattedMortgage);
 
     }
